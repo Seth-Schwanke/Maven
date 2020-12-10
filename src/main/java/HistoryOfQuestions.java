10016c6f021d7c7da2367fc3aede00b970a72ac6
Michael Webb <michaelwebbj@gmail.com>
2020-12-09 19:35:12 -0700

    HQ updated

--- src/main/java/HistoryOfQuestions.java
+++ src/main/java/HistoryOfQuestions.java
@@ -3,17 +3,18 @@ public class HistoryOfQuestions extends RestfulServer {
     public static void main(String[] args) {
     // Get String Question
     String test = "test";
-    ArrayList<String> HQ = new ArrayList<String>();
+    ArrayList<String> ASKED = new ArrayList<String>();
+    ArrayList<String> ANSWERED = new ArrayList<String>();
 
     // Check if question has already been asked
     // If yes return string to RestfulServer
     // If no add to arraylist
-     if(HQ.contains(test)) {
+     if(ASKED.contains(test)) {
         System.out.println("Has already been asked");
         // Return previous Answer
 
     }else{
-        HQ.add(test);
+         ASKED.add(test);
     }
 
     }
